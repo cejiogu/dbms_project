1366ed08900a02bb70990b835c702c7ad3a297ba
Ilan Klimberg <idk7@cornell.edu>
2024-03-25 13:43:18 -0400

    Added checks for Date data type
    - added all_numbers, is_valid_year. is_valid_month_or_day. and is_valid_date functions
    - added documentation for each function as well
    - Problem: column.ml is not recognising Data data type

--- lib/column.ml
+++ lib/column.ml
@@ -1,14 +1,39 @@
+include String
+
 type elem =
   | Int of int
   | Bool of bool
   | Float of float
   | String of string
-  | Date of int * int * int
+  | Date of string * string * string
 
 type t = {
   label : string;
   data : elem list;
 }
+
+let all_numbers (s : string) : bool =
+  if Str.string_match (Str.regexp "[0-9]+$") s 0 then true else false
+
+let is_valid_year (year : string) : bool =
+  if year = "NULL" || all_numbers year then true else false
+
+let is_valid_month_or_day (month_or_day : string) : bool =
+  if
+    month_or_day = "NULL"
+    || (all_numbers month_or_day && String.length month_or_day = 2)
+  then true
+  else false
+
+let is_valid_date (date : Date) =
+  match date with
+  | year, month, day ->
+      if
+        is_valid_year year
+        && is_valid_month_or_day month
+        && is_valid_month_or_day day
+      then true
+      else false
 
 let label t = t.label
 let data t = t.data
--- lib/column.mli
+++ lib/column.mli
@@ -6,7 +6,19 @@ type t
     and [data] - the data of the column which is of type elem list. *)
 
 val label : t -> string
-(** [label t] takes in a type t [t] and returns the label of t *)
+(** [label t] takes in a type t [t] and returns the label of [t] *)
 
 val data : t -> elem list
-(** [data t] takes in a type t [t] and returns the data of t *)
+(** [data t] takes in a type t [t] and returns the data of [t] *)
+
+val all_numbers : string -> bool
+(** [all_numbers s] returns whether or not the string [s] contains only numbers. *)
+
+val is_valid_year : string -> bool
+(** [is_valid_year s] returns whether or not the string [s] is a valid year. *)
+
+val is_valid_month_or_day : string -> bool
+(** [is_valid_month_or_day s] returns whether or not the string [s] is a valid month or day. *)
+
+val is_valid_date : string -> bool
+(** [is_valid_date d] returns whether or not the Date [d] is a valid Date. *)
