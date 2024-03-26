d441b8f3b010355aa20bb24b08bfe0b2d9206b7f
Chimdi <ce248@cornell.edu>
2024-03-26 15:47:21 -0400

    Implement empty_table

--- lib/table.ml
+++ lib/table.ml
@@ -1,5 +1,52 @@
-include Column
+(**Implementation of Table module*)
+module Table = struct
+  open Column
 
-type t = Column.t list
+  exception InvalidQuery of string
 
-let empty () : t = []
+  type column = Column.t
+
+  type table = {
+    table_name : string;
+    columns : column list;
+  }
+
+  let empty_table (name : string) : table =
+    if name = "" then
+      raise (InvalidQuery "You must enter the name of your table!")
+    else
+      let table = { table_name = name; columns = [] } in
+      table
+
+  let select_from (_ : string) (_ : string) = failwith "TODO"
+
+  let rec insert_into (_ : string) (_ : string array) (_ : string array) =
+    failwith "TODO"
+
+  let alter_add (_ : string) (_ : string) (_ : string) = failwith "TODO"
+  let alter_drop (_ : string) (_ : string) = failwith "TODO"
+  let alter_rename (_ : string) (_ : string) (_ : string) = failwith "TODO"
+  let print_table (_ : table) = failwith "TODO"
+end
+
+module type Table = sig
+  exception InvalidQuery of string
+
+  type column
+  type table
+
+  val empty_table : string -> table
+  (**[empty_table] is an empty table of title [string] *)
+
+  val create_table : string -> string list -> table
+  (**[create_table] is a [table] titled [string] and with columns titled after
+     each string in [string list]*)
+
+  val select_from : string -> string -> table
+  (**[select_from] is a [table] representing the column of title [string],
+     selected from the table of title [string]*)
+
+  val insert_into : string -> string array -> string array -> table
+  (**[insert_into] is a [table] containing a new row of values [string] that
+     holds the string values in [string array] *)
+end
