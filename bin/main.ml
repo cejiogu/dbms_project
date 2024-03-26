3b1b14ef1b4a3db52490a91ec777b1ba1d9ea265
Chimdi <ce248@cornell.edu>
2024-03-26 19:53:05 -0400

    Implement temporary bug fix

--- bin/main.ml
+++ bin/main.ml
@@ -1,10 +1,7 @@
-let rec prompt_and_print ()=
-  print_endline "Enter SQL command:";
-  let c= read_line () in
-  match c with
-  | exit-> print_endline "Exited"; ()
-  | _-> (*insert parser call*)
+(* let rec prompt_and_print ()= print_endline "Enter SQL command:"; let c=
+   read_line () in match c with | exit-> print_endline "Exited"; () | _->
+   (*insert parser call*)
 
-  ;prompt_and_print ()
+   ;prompt_and_print ()
 
-let ()=promt_and_print ()
+   let ()=promt_and_print () *)
