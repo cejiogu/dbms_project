b0c119930035abad0325a9e806fc086aa2b2052d
Chimdi <ce248@cornell.edu>
2024-03-28 03:36:49 -0400

    Remove SCHEMA functionality, modify program display

--- bin/main.ml
+++ bin/main.ml
@@ -13,9 +13,18 @@ module StringBuilder = struct
     | _ -> raise (ArgumentError "You did not enter enough arguments")
 end
 
+let error_message () =
+  let () = print_endline "Error: Invalid Command" in
+  let () =
+    print_endline "You did not enter a valid command. Please try again."
+  in
+  let () = print_endline "" in
+  ()
+
 let cycler () =
   let () = print_endline "Enter an SQL command to modify your database: " in
   let command = read_line () in
+  let () = print_endline "" in
   command
 
 let rec prompt_loop (exit : string list) (input : string)
@@ -28,34 +37,34 @@ let rec prompt_loop (exit : string list) (input : string)
     match command with
     | [] ->
         let () = print_endline "You did not provide any commands" in
+        let () = print_endline "" in
         prompt_loop exit (cycler ()) database
-    | _ :: [] ->
-        if command = [ "SCHEMA" ] then
-          let () = print_endline database.name in
-          prompt_loop exit (cycler ()) database
-        else prompt_loop exit (cycler ()) database
-    | [ _; _ ] ->
-        let first_word = List.nth command 0 in
-        let second_word = List.nth command 1 in
-        if first_word = "CREATE" && second_word = "TABLE" then
+    | word1 :: word2 :: _ ->
+        if word1 = "CREATE" && word2 = "TABLE" then
           let columns = StringBuilder.third_onward command in
           let database =
             Final_project.Database.insert_table database (List.nth command 2)
               columns
           in
           prompt_loop exit (cycler ()) database
-        else print_endline "Invalid Command; Terminating Program"
-    | _ -> prompt_loop exit (cycler ()) database
+        else
+          let () = error_message () in
+          prompt_loop exit (cycler ()) database
+    | _ ->
+        let () = error_message () in
+        prompt_loop exit (cycler ()) database
 
 let initial_prompt =
   let exit = [ "exit"; "quit" ] in
   let () = print_endline "Enter the name of your database: " in
   let name = read_line () in
+  let () = print_endline "" in
   if List.mem name exit then print_endline "You have quit the program"
   else
     let database = Final_project.Database.empty_database name in
     let () = print_endline "Enter an SQL command to modify your database: " in
     let command = read_line () in
+    let () = print_endline "" in
     prompt_loop exit command database
 
 let () = initial_prompt
