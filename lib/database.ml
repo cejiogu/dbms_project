8c6aef31d4bb8be0ec8487e54ea7b1176044600f
Chimdi <ce248@cornell.edu>
2024-03-28 02:48:30 -0400

    Uncomment files, resolve bug fixes

--- lib/database.ml
+++ lib/database.ml
@@ -1,18 +1,29 @@
-(* include Table
+include Table
 
-   exception InvalidQuery of string
+exception InvalidQuery of string
 
-   type table = Table.t
+type table = Table.t
 
-   type t = { name : string; tables : table list; }
+type t = {
+  name : string;
+  tables : table list;
+}
 
-   let empty_database (name : string) : t = if name = "" then raise
-   (InvalidQuery "You must enter the name of your table!") else let database = {
-   name; tables = [] } in database
+let name t = t.name
+let tables t = t.tables
 
-   let table_exists (name : string) (db : t) : bool = List.exists (fun t ->
-   Table.table_name t = name) db.tables
+let empty_database (name : string) : t =
+  if name = "" then
+    raise (InvalidQuery "You must enter the name of your table!")
+  else
+    let database = { name; tables = [] } in
+    database
 
-   let insert_table (db : t) (name : string) (columns : string list) : t = if
-   table_exists name db then db else let new_table = Table.create_table name
-   columns in { db with tables = new_table :: db.tables } *)
+let table_exists (name : string) (db : t) : bool =
+  List.exists (fun t -> Table.table_name t = name) db.tables
+
+let insert_table (db : t) (name : string) (columns : string list) : t =
+  if table_exists name db then db
+  else
+    let new_table = Table.create_table name columns in
+    { db with tables = new_table :: db.tables }
--- lib/database.mli
+++ lib/database.mli
@@ -1,31 +1,44 @@
-(* exception InvalidQuery of string
+exception InvalidQuery of string
 
 type table
 (**[table] is the type of database tables. *)
 
-type t
+type t = {
+  name : string;
+  tables : table list;
+}
 (**[t] is an alias for the type of a database. *)
 
+val name : t -> string
+(**[name] is the name [string] of a database [t]*)
+
+val tables : t -> table list
+(**[tables] is the tables in a database [t]*)
+
 val empty_database : string -> t
-(**[empty_database] is an empty database, titled [string]. Raises
-   [InvalidQuery] if [string] is empty. *)
+(**[empty_database] is an empty database, titled [string]. Raises [InvalidQuery]
+   if [string] is empty. *)
+
+val table_exists : string -> t -> bool
+(**[table_exists] is whether or not a table of title [string] exists in [t]*)
 
 val insert_table : t -> string -> string list -> t
-(* future implementation of insert_table below. Current implementation of insert_table above. *)
-(* val insert_table : t -> string -> (string * string) list -> t *)
 (**[insert_table] is a [database] that now includes a new table titled [string]
    and with columns titled after each string in [string list] into [database].
    Does not change [database] if a table titled [string] already exists in
    [database]. This function corresponds to the SQL CREATE TABLE statement. *)
 
+(* future implementation of insert_table below. Current implementation of
+   insert_table above. *)
+(* val insert_table : t -> string -> (string * string) list -> t *)
+
 (* val select_from : string -> table -> table *)
-(**[select_from] is a [table] representing the column, titled [string],
-   selected from [table]. Raises [InvalidQuery] if the [string] title of the
-   column is not found in [table]. This function corresponds to the SQL SELECT
-   statement. *)
+(**[select_from] is a [table] representing the column, titled [string], selected
+   from [table]. Raises [InvalidQuery] if the [string] title of the column is
+   not found in [table]. This function corresponds to the SQL SELECT statement. *)
 
 (* val insert_into : table -> string list -> string list -> table *)
 (**[insert_into] is a [table] that inserts into the columns whose names are
    speficied in the first [string list] argument a new row of data, which is
-   specified in the second [string list] argument. This function corresponds
-   to the SQL INSERT INTO statement. *) *)
+   specified in the second [string list] argument. This function corresponds to
+   the SQL INSERT INTO statement. *)
