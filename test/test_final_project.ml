909086ec9463fc66c02e09b927d04fb77563d8bf
Ilan Klimberg <idk7@cornell.edu>
2024-04-03 01:19:45 -0400

    Added more tests
    - looking good!

--- test/test_final_project.ml
+++ test/test_final_project.ml
@@ -17,18 +17,32 @@ let tests_column =
              (Column.data (Column.make "transport" [ "bus" ])) );
        ]
 
+(* Table1: *)
 let t1 = Table.make "IntegerTable" [ "ID"; "Value" ]
 let t1_insert1 = Table.insert_into t1 [ "ID"; "Value" ] [ "1"; "100" ]
 let t1_insert2 = Table.insert_into t1_insert1 [ "ID"; "Value" ] [ "2"; "200" ]
 
-(* let table2 = let t = Table.make "StringTable" [ "Name"; "Occupation" ] in let
-   t1 = Table.insert_into t [ "Name"; "Occupation" ] [ "Alice"; "Engineer" ] in
-   Table.insert_into t1 [ "Name"; "Occupation" ] [ "Bob"; "Doctor" ]
+(* Table2: *)
+let t2 = Table.make "StringTable" [ "Name"; "Occupation" ]
 
-   let table3 = let t = Table.make "MixedTable" [ "ID"; "Name"; "Birthday" ] in
-   let t1 = Table.insert_into t [ "ID"; "Name"; "Birthday" ] [ "1"; "Charlie";
-   "1990-01-01" ] in Table.insert_into t1 [ "ID"; "Name"; "Birthday" ] [ "2";
-   "Dana"; "1985-05-23" ] *)
+let t2_insert1 =
+  Table.insert_into t2 [ "Name"; "Occupation" ] [ "Alice"; "Engineer" ]
+
+let t2_insert2 =
+  Table.insert_into t2_insert1 [ "Name"; "Occupation" ] [ "Bob"; "Doctor" ]
+
+(* Table3: *)
+let t3 = Table.make "MixedTable" [ "ID"; "Name"; "Birthday" ]
+
+let t3_insert1 =
+  Table.insert_into t3
+    [ "ID"; "Name"; "Birthday" ]
+    [ "1"; "Charlie"; "1990-01-01" ]
+
+let t3_insert2 =
+  Table.insert_into t3_insert1
+    [ "ID"; "Name"; "Birthday" ]
+    [ "2"; "Dana"; "1985-05-23" ]
 
 let tests_table =
   "test Table"
@@ -40,11 +54,37 @@ let tests_table =
              (Table.string_of_table t1) );
          ( "Insert into table1" >:: fun _ ->
            assert_equal "Table: IntegerTable\n{ID, [1]}\n{Value, [100]}\n"
-             (Table.string_of_table t1_insert1) );
-         ( "Insert into table1 again" >:: fun _ ->
+             (Table.string_of_table t1_insert1);
            assert_equal
              "Table: IntegerTable\n{ID, [2, 1]}\n{Value, [200, 100]}\n"
              (Table.string_of_table t1_insert2) );
+         ( "Table2 test" >:: fun _ ->
+           assert_equal "Table: StringTable\n{Name, []}\n{Occupation, []}\n"
+             (Table.string_of_table t2);
+           assert_equal
+             "Table: StringTable\n{Name, [Alice]}\n{Occupation, [Engineer]}\n"
+             (Table.string_of_table t2_insert1);
+           assert_equal
+             "Table: StringTable\n\
+              {Name, [Bob, Alice]}\n\
+              {Occupation, [Doctor, Engineer]}\n"
+             (Table.string_of_table t2_insert2) );
+         ( "Table3 test" >:: fun _ ->
+           assert_equal
+             "Table: MixedTable\n{ID, []}\n{Name, []}\n{Birthday, []}\n"
+             (Table.string_of_table t3);
+           assert_equal
+             "Table: MixedTable\n\
+              {ID, [1]}\n\
+              {Name, [Charlie]}\n\
+              {Birthday, [1990-01-01]}\n"
+             (Table.string_of_table t3_insert1);
+           assert_equal
+             "Table: MixedTable\n\
+              {ID, [2, 1]}\n\
+              {Name, [Dana, Charlie]}\n\
+              {Birthday, [1985-05-23, 1990-01-01]}\n"
+             (Table.string_of_table t3_insert2) );
        ]
 
 let _ = run_test_tt_main tests_column
