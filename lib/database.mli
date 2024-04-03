d6708ce310b51c370227b7a39dc2b3321b0beacc
Chimdi <ce248@cornell.edu>
2024-04-03 10:39:17 -0400

    Modify specifications in interface of database

--- lib/database.mli
+++ lib/database.mli
@@ -4,15 +4,16 @@ type table
 (** The type representing a table within a database. *)
 
 type t
-(** The type representing a database, which consists of a name and a list of tables. *)
+(** The type representing a database, which consists of a name and a list of
+    tables. *)
 
 val name : t -> string
 (** [name db] Returns the name of the database [db].
-      @param db The database instance whose name is to be retrieved. *)
+    @param db The database instance whose name is to be retrieved. *)
 
 val tables : t -> table list
 (** [tables db] Returns the list of tables in the database [db].
-      @param db The database instance whose tables are to be retrieved. *)
+    @param db The database instance whose tables are to be retrieved. *)
 
 val empty : string -> t
 (** [empty_database name] Creates an empty database with the specified name [name].
@@ -29,9 +30,10 @@ val table_exists : string -> t -> bool
       @notes This function can be used to prevent duplicate tables in a database. *)
 
 val insert_table : t -> string -> string list -> t
-(** [insert_table db name columns] Creates a new table with the specified name [name] and columns [columns] and inserts it into the database [db].
-      @param db The database into which the new table is to be inserted.
-      @param name The name of the new table.
-      @param columns A list of names for each column in the new table.
-      @return An updated database containing the new table.
-      @raise Does not change the database if a table with the specified name already exists. *)
+(** [insert_table db name columns] Creates a new table with the specified name
+    [name] and columns [columns] and inserts it into the database [db].
+    @param db The database into which the new table is to be inserted.
+    @param name The name of the new table.
+    @param columns A list of names for each column in the new table.
+    @return An updated database containing the new table.
+    @note Does not change the database if a table with the specified name already exists. *)
