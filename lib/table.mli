6c7bde1c28c97467e9d93384af8cd9a8e059b84e
Chimdi <ce248@cornell.edu>
2024-04-02 22:37:18 -0400

    Modify return value of function, modify print specification

--- lib/table.mli
+++ lib/table.mli
@@ -1,10 +1,11 @@
 exception InvalidQuery of string
-  
+
 type column
 (** The type representing a single column in a table. *)
 
 type t
-(** The type representing a table, which consists of a name [name] and a list of columns [columns]. *)
+(** The type representing a table, which consists of a name [name] and a list of
+    columns [columns]. *)
 
 val name : t -> string
 (** [name t] Returns the name of the table [t].
@@ -13,7 +14,7 @@ val name : t -> string
 val columns : t -> column list
 (** [columns t] Returns the list of columns in the table [t].
     @param t The table whose name you're retrieving. *)
-  
+
 val empty : string -> t
 (** [empty name] Creates an empty table with the specified name [name].
     @param name The name of the table to be created.
@@ -26,7 +27,7 @@ val make : string -> string list -> t
     @param col_names A list of names for each column in the table.
     @return A new table instance with the specified name and columns.
     @note This function is analogous to the SQL CREATE TABLE statement. *)
-  
+
 val insert_into : string -> string list -> string list -> t -> 'a
 (** [insert_into tab_name col_names values table] Inserts a new row into [table], with values specified in [values] corresponding to the columns named in [column_names].
     @param tab_name The name of the table where values are to be inserted.
@@ -36,8 +37,9 @@ val insert_into : string -> string list -> string list -> t -> 'a
     @raise InvalidQuery if the lengths of [col_names] and [values] do not match.
     @return An updated table with the new row of values inserted.
     @notes Requires implementation of functionality from the Column module to properly add values to columns. *)
-  
-val print : t -> unit
+
+val print : t -> string list list
 (** [print t] Prints the table [t] to the terminal, showing its structure and content.
     @param t The table to be printed.
+    @return The table converted to a 2-dimensional list of strings 
     @notes The actual implementation should iterate over the table's rows and columns, formatting the output for readability. *)
