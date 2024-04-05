9d9d512e666fb8abd94b5288a237b25f4f7b45a8
Chimdi <ce248@cornell.edu>
2024-04-05 04:13:09 -0400

    Implement get_table function, commented out for now

--- lib/database.ml
+++ lib/database.ml
@@ -20,7 +20,7 @@ let empty (name : string) : t =
     database
 
 let table_exists (name : string) (db : t) : bool =
-  List.exists (fun t -> Table.name t = name) db.tables
+  List.exists (fun t -> Table.title t = name) db.tables
 
 let insert_table (db : t) (name : string) (column_names : string list)
     (column_types : string list) : t =
@@ -28,3 +28,12 @@ let insert_table (db : t) (name : string) (column_names : string list)
   else
     let new_table = Table.make name column_names column_types in
     { db with tables = new_table :: db.tables }
+
+(* let rec get_table_aux (tables : table list) (name : string) : table = match
+   tables with | [] -> failwith "Should never come to this case" | (h : table)
+   :: (t : table list) -> if Table.title h = name then h else get_table_aux t
+   name *)
+
+(* let get_table (db : t) (name : string) = if table_exists name db then
+   get_table_aux db.tables name else raise (InvalidQuery ("The table titled " ^
+   name ^ " does not exist ")) *)
